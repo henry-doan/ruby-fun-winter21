1a80ec0d226d0073d0abd1d16f61ff5c37e6296f
Henry Doan <henrydoan96@gmail.com>
2021-11-10 00:23:51 -0700

    end of day 1

---
+++ notes.rb
@@ -0,0 +1,114 @@
+# Object - Noun
+# OOP - Object oriented programming 
+
+# Data Types 
+# Boolean
+# true false
+# 1  0
+# strings 
+# "" ''
+# "bob"
+# "2"
+# "true"
+# 'bob'
+# # 'don't'
+# "don't"
+# hash 
+#   {key: value}
+#   {first_name: "bob", age: 4}
+# array
+#   [1,2,3,4, "adsf", []]
+# integer 
+# 0
+# 1212
+# -123434
+# Float
+# 1.23
+# -1.1232341234134
+# nil 
+# no value 
+# undefined
+# not made yet
+# NAN
+# Not a number
+
+# Math
+# + - * / 
+# pemas
+# % - modulus 
+# give remainder 
+# puts 3 % 5
+
+# puts 2 - 2.2
+# puts 1 / 2
+# 0.5
+# 0
+# string concatenation
+# puts  'hello' + ' world' + '!'
+# = - Assignment
+# == - equality, by value 
+# 3 == "3"
+# === - equality, value and data type
+# 3 === "3"
+# != -  not equal 
+# ! - bang, opposite or not
+# !== - not equal by value and data type
+# var - store objects to reference later
+# var_name = obj 
+# first_name = "bob"
+# puts first_name
+# name of var, one word, connected _ 
+# descriptive
+# avoid key word reserved word, 
+# puts = "bob"
+
+# x = "bob"
+# puts x 
+
+# Scope - where you have access to the variable
+# Constant 
+#   capital first letter 
+#   Foo = "bob"
+#   scoped to the file 
+#   can change but shouldn't change
+
+# local 
+#   lowercase first letter 
+#   foo = 3
+#   scoped with a code block 
+#   if it has a end or a {} that is a block
+  # def method1 
+  #   first_name = "gog"
+  # end
+  # method1
+  # puts first_name
+
+# instance 
+  # @foo = "bob"
+  # @ at the front
+  # scope to the class , running instance of the class 
+  # class Person
+
+  # end
+# class 
+  # @@foo = "bob"
+  # @@ at the front 
+  # scoped to the class and or file
+
+# *do not use 
+# global
+# $foo = "bob"
+# $ at the front 
+# scoped throughout a machine
+
+# planet = "mars"
+# string interpolation
+# need to have double ""
+#{}
+# puts "hello I am on #{planet}"
+
+# puts "hello I am on " + planet
+
+# puts "what is your name?"
+# first_name = "jill"
+# puts "Hello and welcome #{first_name}"
